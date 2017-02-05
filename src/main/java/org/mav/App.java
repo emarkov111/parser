ac334009f0aa44f27490dad9585a48527bc04d2b
Andrew Markovkin <emarkov111@gmail.com>
2017-02-05 23:38:36 +0200

    two

--- src/main/java/org/mav/App.java
+++ src/main/java/org/mav/App.java
@@ -6,7 +6,7 @@ package org.mav;
 public class App {
     public static void main(String[] args) {
         Parser parser = new Parser("https://www.us-proxy.org");
-        parser.getTitle();
-        parser.getTable();
+//        parser.getTitle();
+        parser.getProxyFile();
     }
 }
--- src/main/java/org/mav/Parser.java
+++ src/main/java/org/mav/Parser.java
@@ -1,34 +1,50 @@
 package org.mav;
 
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Created by andrew on 05.02.17.
+ *
  */
 public class Parser {
     private Document doc = null;
+    private FileWriter file = null;
 
     public Parser(String url) {
         try {
             doc = Jsoup.connect(url).get();
+            file = new FileWriter("/home/andrew/developer/projects/java/parser/proxy.json");
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
     public String getTitle() {
         String title = doc.title();
-        System.out.println(title);
         return title;
     }
 
-    public void getTable() {
-        Elements title = doc.getElementsByClass("display fpltable");
-        System.out.println(title);
+    public void getProxyFile() {
+        Element table = doc.select("table").get(0); //select the first table.
+        Elements rows = table.select("tr");
+        JSONObject obj = new JSONObject();
+        for (int i = 1; i < rows.size() - 1; i++) { //first row is the col names so skip it.
+            Element row = rows.get(i);
+            Elements cols = row.select("td");
+            obj.put(cols.get(0).text(), cols.get(1).text());
+        }
+        try {
+            file.write(obj.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
